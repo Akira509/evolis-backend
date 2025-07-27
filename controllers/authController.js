0a0e2dce3f6c94b7515d95b1a6a66d640964d250
Priyanka <mukulkadyan202@gmail.com>
2025-07-27 13:33:17 +0530

    Removed node_modules and added .gitignore

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "Email already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+
+    await user.save();
+    res.status(201).json({ message: "Signup successful" });
+  } catch (err) {
+    res.status(500).json({ message: "Error signing up", error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ error: "Invalid email" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return res.status(400).json({ error: "Invalid password" });
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res
+      .cookie("token", token, { httpOnly: true })
+      .status(200)
+      .json({
+        message: "Login successful",
+        token,
+        user: {
+          name: user.name,
+          email: user.email,
+        },
+      });
+  } catch (err) {
+    console.error("Login Error:", err.message);
+    res.status(500).json({ error: "Server error" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+import chatRoutes from "./routes/chat.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// middleware
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.use("/api", chatRoutes);
+app.use("/api", authRoutes);
+
+// connect to DB and start server
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected with Database!");
+    console.log("Using DB:", mongoose.connection.name);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to DB", err.message);
+    process.exit(1);
+  }
+};
+
+startServer();
