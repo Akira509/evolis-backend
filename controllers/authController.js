7510919da3b0fea0b86dafb6376589be613119ac
Akira509 <priyakadyan509@gmail.com>
2025-07-28 23:51:06 +0530

    Fix CORS and cookie settings for cross-origin login

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,12 @@ export const login = async (req, res) => {
     });
 
     res
-      .cookie("token", token, { httpOnly: true })
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 24 * 60 * 60 * 1000,
+      })
       .status(200)
       .json({
         message: "Login successful",
